d4998c1b0a8d2ecd09b3416b64537cb568135416
rofernan42 <romain.fndz42@gmail.com>
2021-07-27 16:36:46 +0200

    first push

---
+++ back/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const MONGODB_URI = 'mongodb+srv://romain:LUJODAVfeltMTpKv@cluster0.ub4t7.mongodb.net/blog?retryWrites=true&w=majority'
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        const dateStr = new Date().toISOString().replace(/:/g, '-');
+        cb(null, dateStr + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+// app.use(bodyParser.json()); // application/json - deprecated
+app.use(express.json()); // application/json
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // allow every domains
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        app.listen(8080);
+    })
